1099450f059be43c87ce0b077d6acb572c65d926
PallaviKatari <pallavikatari@gmail.com>
2022-07-04 15:41:42 +0530

    Commit_4/7/22

--- src/Components/YourCourseList.jsx
+++ src/Components/YourCourseList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import "../Styles/Career.css";
 
-export default class MyCourseList extends Component {
+class CourseDetails extends Component {
 
     state = { count: 0 }
 
@@ -25,43 +25,50 @@ export default class MyCourseList extends Component {
 
     render() {
         return (
-            <div>
+            <>
+                No of Participants: {this.state.count}
+                <br></br>
+                <button onClick={this.handleDecrement} style={{ color: 'blue' }}>-</button>
+                <button onClick={this.handleIncrement} style={{ color: 'blue' }}>+</button>
+            </>
+        )
+    }
+}
+
+const course = [
+    {
+        id: 1,
+        name: 'DOTNET'
+    },
+    {
+        id: 2,
+        name: 'JAVA'
+    },
+    {
+        id: 3,
+        name: 'PHP'
+    },
+    {
+        id: 4,
+        name: 'PYTHON'
+    },
+
+];
+
+export default function MyCourseList() {
+    return (
+        <>
             {course.map(course => (
                 <div className="divtag" key={course.id}>
                     <>
                         {course.name}
                         <br></br>
-                        No of Participants: {this.state.count}
-                        <br></br>
-                        <button onClick={this.handleDecrement} style={{color:'blue'}}>-</button>
-                        <button onClick={this.handleIncrement} style={{color:'blue'}}>+</button>
+                        <CourseDetails />
                     </>
                 </div>
             )
             )}
-        </div>
-        )
-    }
+        </>
+    )
 }
 
-  const course=[
-        {
-            id:1,
-            name:'DOTNET'
-        },
-        {
-            id:2,
-            name:'JAVA'
-        },
-        {
-            id:3,
-            name:'PHP'
-        },
-        {
-            id:4,
-            name:'PYTHON'
-        },
-
-    ];
-
-    
